4e6c7ae807dbc794e9d0ef0e8ee26797b1d488fb
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2020-12-29 17:54:12 +0100

    Modified Subject

--- compare_dirs.py
+++ compare_dirs.py
@@ -1,8 +1,9 @@
 #!/opt/envs/pcloud/bin/python3
 """
-This script will analyze the inventory of pcloud.
+This script compares a directory on PCloud with a directory on the local host..
 """
 
+import argparse
 import json
 import logging
 import os
@@ -10,46 +11,70 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from lib import my_env
+from pprint import pprint
 
 
 def item2key(pc_dict, path, contents):
     for item in contents:
         fn = f"{path}/{item['name']}"
         if item['isfolder']:
-            pc_dict[fn] = dict(
-                isfolder=item['isfolder'],
-                created=item['created'],
-                modified=item['modified']
-            )
+            if source_dir in fn:
+                pc_dict[fn] = dict(
+                    isfolder=item['isfolder'],
+                    created=item['created'],
+                    modified=item['modified']
+                )
             item2key(pc_dict, f"{path}/{item['name']}", item['contents'])
         else:
-            pc_dict[fn] = dict(
-                isfolder=item['isfolder'],
-                created=item['created'],
-                modified=item['modified'],
-                fileid=item['fileid'],
-                size=item['size'],
-                hash=item['hash'],
-                contenttype=item['contenttype']
+            if source_dir in fn:
+                pc_dict[fn] = dict(
+                    isfolder=item['isfolder'],
+                    created=item['created'],
+                    modified=item['modified'],
+                    fileid=item['fileid'],
+                    size=item['size'],
+                    hash=item['hash'],
+                    contenttype=item['contenttype']
+                )
+    return
+
+
+def get_local_pc(local_dict, local_path):
+    for root, dirs, files in os.walk(local_path):
+        fn = root
+        local_dict[root] = dict(isfolder=True)
+        for file in files:
+            local_dict[file] = dict(
+                isfolder=False,
+                size=os.path.getsize(os.path.join(root, file))
             )
     return
 
+parser = argparse.ArgumentParser(
+    description="Compare source (PCloud) and target (Local) directories."
+)
+parser.add_argument('-s', '--source_dir', type=str, required=True,
+                    help='Please provide the PCloud source directory.')
+parser.add_argument('-t', '--target_dir', type=str, required=True,
+                    help='Please provide the Local target directory ID.')
+args = parser.parse_args()
 cfg = my_env.init_env("pcloud", __file__)
 logging.info("Start application")
-pc_prev = {}
-pc_current = {}
+logging.info("Arguments: {a}".format(a=args))
+source_dir = args.source_dir
+pc_tree = {}
 fp = os.getenv('DATADIR')
 inventory_files = [file for file in os.listdir(fp) if '.json' == file[-len('.json'):]]
 inventory_files.sort(reverse=True)
-[ffn_current, ffn_prev] = inventory_files[:2]
-# ffn = '/home/dirk/development/python/pcloud/data/pcloud20201227161239.json'
+ffn_current = inventory_files[0]
 with open(os.path.join(fp, ffn_current), 'r') as fh:
     pc_contents = json.load(fh)
-item2key(pc_current, pc_contents['path'], pc_contents['contents'])
-# ffn = '/home/dirk/development/python/pcloud/data/pcloud20201227144214.json'
-with open(os.path.join(fp, ffn_prev), 'r') as fh:
-    pc_contents = json.load(fh)
-item2key(pc_prev, pc_contents['path'], pc_contents['contents'])
+item2key(pc_tree, pc_contents['path'], pc_contents['contents'])
+pprint(pc_tree)
+local_tree = {}
+get_local_pc(local_tree, args.target_dir)
+pprint(local_tree)
+raise Exception('OK for Now')
 new_items = []
 modified_items = []
 removed_items = []
@@ -81,7 +106,7 @@ gmail_pwd = os.getenv('GMAIL_PWD')
 recipient = os.getenv('RECIPIENT')
 
 msg = MIMEMultipart()
-msg["Subject"] = f"PCloud: {len(new_items)} New - {len(modified_items)} Modified - {len(removed_items)} Removed"
+msg["Subject"] = "PCloud Report"
 msg["From"] = gmail_user
 msg["To"] = recipient
 
